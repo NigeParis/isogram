b427d5e8cdf28ee6739194930a4d4fe1f192e38b
Nige 42 <nige42@iMac-de-Nige.local>
2023-07-13 10:37:34 +0200

    new version with an index alphabet array test

--- isogram.c
+++ isogram.c
@@ -6,7 +6,7 @@
 /*   By: 42_Legin <Nige@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/04 00:10:34 by 42_Legin          #+#    #+#             */
-/*   Updated: 2023/06/16 18:31:39 by 42_Legin         ###   ########.fr       */
+/*   Updated: 2023/07/13 10:35:05 by nige42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 static bool	_valid_char(char current_char);
 static char	_to_lowercase( char current_char);
-static bool	_found_double_char(const char *char_in_string, char current_char);
+static void	set_to_zero_alphabet_index(int *ptr);
 
 /**
 *	main function 
@@ -23,48 +23,34 @@ static bool	_found_double_char(const char *char_in_string, char current_char);
 
 bool	is_isogram(const char phrase[])
 {
-	const char	*position_in_phrase;
 	char		current_char;
+	int			arr_alphabet_index[26];
+	int			index;
 
-	position_in_phrase = phrase;
-	if (!phrase)
+	index = 0;
+	if ((!phrase) || (phrase[0] == '\0'))
 		return (false);
-	while (*position_in_phrase)
+	set_to_zero_alphabet_index(arr_alphabet_index);
+	while (*phrase)
 	{
-		while ((*position_in_phrase == ' ') || (*position_in_phrase == '-'))
-			position_in_phrase++;
-		if (*position_in_phrase == '\0')
+		while ((*phrase == ' ') || (*phrase == '-'))
+			phrase++;
+		if (*phrase == '\0')
 			break ;
-		if (!_valid_char(*position_in_phrase))
+		if (!_valid_char(*phrase))
 			return (false);
-		current_char = _to_lowercase(*position_in_phrase);
-		if (_found_double_char((position_in_phrase + 1), current_char))
+		current_char = _to_lowercase(*phrase);
+		index = (current_char - 97);
+		arr_alphabet_index[index] = arr_alphabet_index[index] + 1;
+		if (arr_alphabet_index[index] > 1)
 			return (false);
-		position_in_phrase++;
+		phrase++;
 	}
 	return (true);
 }
 
-static bool	_found_double_char(const char *char_in_string, char current_char)
-{
-	char	c;
-	int		count;
-
-	count = 0;
-	while (*char_in_string)
-	{
-		c = *char_in_string;
-		c = _to_lowercase(*char_in_string);
-		count += (current_char == c);
-		if (count > 0)
-			return (true);
-		char_in_string++;
-	}
-	return (false);
-}
-
 static bool	_valid_char(char current_char)
-{	
+{
 	current_char = _to_lowercase(current_char);
 	return ((current_char >= 'a') && (current_char <= 'z'));
 }
@@ -74,4 +60,19 @@ static char	_to_lowercase( char current_char)
 	if ((current_char >= 'A') && (current_char <= 'Z'))
 		current_char = current_char + 32;
 	return (current_char);
+}
+
+static void	set_to_zero_alphabet_index(int *ptr)
+{
+	int	alphabet;
+	int	count;
+
+	alphabet = 26;
+	count = 0;
+	while (count < alphabet)
+	{
+		*ptr = 0;
+		ptr++;
+		count++;
+	}
 }
